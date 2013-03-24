35bd193ace600c82ed3133ad28261162f6b58195
Andy Park <sohocoke@gmail.com>
2013-03-24 22:11:39 +0100

    UIWebView#load_bundle_file and other improvements.

--- app/browser.rb
+++ app/browser.rb
@@ -21,6 +21,7 @@ class BrowserViewController < MotionViewController
     when /^js:/
       result = @web_view.stringByEvaluatingJavaScriptFromString input.gsub(/^js:/, '')
       puts result
+      # NOTE result should strictly be a string.
 
       # @web_view.js_alert result
     else
@@ -34,13 +35,24 @@ end
 
 
 class UIWebView
+  def load_bundle_file(name, extension = 'html')
+    url = NSBundle.mainBundle.URLForResource(name, withExtension:extension, subdirectory:nil)
+    self.load_url url
+  end
+
   def load_url( url )
-    url_obj = NSURL.URLWithString url
+    case url
+    when NSURL
+      url_obj =  url
+    else
+      url_obj = NSURL.URLWithString url
+    end
+
     req = NSURLRequest.requestWithURL url_obj
     self.loadRequest req
   end
   
   def js_alert( js )
-    self.stringByEvaluatingJavaScriptFromString "alert(#{js})"
+    self.stringByEvaluatingJavaScriptFromString "alert(#{js});"
   end
 end
