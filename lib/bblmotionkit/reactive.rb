4296024890a81b5f367df44b1f9498aca866df8f
Andy Park <sohocoke@gmail.com>
2013-09-04 04:40:25 +0200

    exception handling

--- lib/bblmotionkit/reactive.rb
+++ lib/bblmotionkit/reactive.rb
@@ -6,8 +6,14 @@ module Reactive
     key_paths.each do |key_path|
       observe self, key_path do |k,c,ctx|
         vals = key_paths.map {|p| self.kvc_get p}
+        # TODO by default, invoke only if value changed.
         pe_debug "#{self}: #{key_path} changed, reacting with #{vals}"
-        block.call(*vals)
+        
+        begin
+          block.call(*vals)
+        rescue Exception => e
+          pe_warn e          
+        end
       end
     end
 
