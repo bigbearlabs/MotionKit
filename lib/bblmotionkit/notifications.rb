9748895762c3c472c53aa119af2e8da4d59173be
Andy Park <sohocoke@gmail.com>
2013-09-05 17:13:36 +0200

    improve for anchovy 1.1

--- lib/bblmotionkit/notifications.rb
+++ lib/bblmotionkit/notifications.rb
@@ -2,53 +2,56 @@
 
 module Notifications
 
-  def notify_in( time_interval = 10, message = 'Alert', owner = self)
+  def notify_in( time_interval = 10, message = 'Notification', badge_count = nil, owner = self)
     since = Time.now
 
     application = UIApplication.sharedApplication
 
     notification = UILocalNotification.alloc.init
-    notification.userInfo = { "owner" => 'owner' }
+    notification.userInfo = { "owner" => owner.to_s }
     notification.fireDate = NSDate.dateWithTimeIntervalSinceNow(time_interval)
     notification.alertBody = message
     notification.alertAction = "View Timer"
-    notification.applicationIconBadgeNumber = [badge_number, 0].max + 1
-    # TODO dismiss button label
+    notification.applicationIconBadgeNumber = badge_count if badge_count
+
+    # TODO set dismiss button message.
 
     application.scheduleLocalNotification(notification)
 
-    pe_debug "scheduled #{notification.inspect}"
+    pe_log "scheduled #{notification} with badge_count #{badge_count}, owner #{owner.to_s}" 
+
     notification
   end
 
   def cancel_notification( notification )
-     UIApplication.sharedApplication.cancelLocalNotification(notification)
-    pe_debug "cancelled #{notification.inspect}"
+    UIApplication.sharedApplication.cancelLocalNotification(notification)
+
+    pe_log "cancelled #{notification}"
   end
 
   def dismiss_notification( notification )
     cancel_notification notification
 
     if notification.fireDate.is_past
-      pe_debug "dismissing #{notification}."
+      pe_log "dismissing #{notification}."
 
-      self.badge_number = badge_number - 1
+      self.badge_count = badge_count - 1
     end
   end
 
   def notifications_for owner
     app.scheduledLocalNotifications.select do |notif|
-      notif.userInfo && notif.userInfo["owner"] == 'owner'
+      notif.userInfo && notif.userInfo["owner"] == owner.to_s
     end
   end
   
   #=
 
-  def badge_number
+  def badge_count
     app.applicationIconBadgeNumber
   end
   
-  def badge_number= number
+  def badge_count= number
     app.applicationIconBadgeNumber = number
   end
   
@@ -59,4 +62,12 @@ class NSDate
   def is_past
     self.compare(NSDate.date) == NSOrderedAscending
   end
+end
+
+
+class UILocalNotification
+  # NOTE disabled due to annoyingly weird crashes involving plist and CFNull type.
+  # def to_s
+  #   self.inspect
+  # end
 end
