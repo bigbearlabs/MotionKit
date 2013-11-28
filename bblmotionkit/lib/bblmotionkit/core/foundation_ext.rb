8caf94f8764b3a1c83ca24f8f973f3b6f635faf7
Andy Park <sohocoke@gmail.com>
2013-11-28 17:47:57 +0100

    cleanup.

--- bblmotionkit/lib/bblmotionkit/core/foundation_ext.rb
+++ bblmotionkit/lib/bblmotionkit/core/foundation_ext.rb
@@ -1,27 +1,3 @@
-# stub
-
-def pe_debug(msg)
-  # pe_log msg
-  puts "DEBUG: #{msg}" if $DEBUG
-end
-
-def pe_log(msg)
-  puts msg
-end
-
-def pe_warn(msg)
-  puts msg
-end
-
-def pe_report(*args)
-  puts args
-end
-
-def debug( *args )
-  pe_debug args.to_s
-end
-
-
 class NSThread
   def self.is_main?
     self.currentThread == NSThread.mainThread
