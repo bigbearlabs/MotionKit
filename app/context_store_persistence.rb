02d1df69e648b3cf121dba81945b7243d8dabbf3
Andy Park <sohocoke@gmail.com>
2013-12-30 14:55:47 +0100

    revise basic version of #save_stacks.

--- app/context_store_persistence.rb
+++ app/context_store_persistence.rb
@@ -83,11 +83,43 @@ end
 module CoreDataPersistence
   def save_stacks
     # Stack -> CoreDataStack, then save.
+
+    persistable_stacks = self.stacks.map do |stack|
+      persistable_pages = persistable_pages stack.pages
+      persistable = CoreDataStack.new name:stack.name, pages:persistable_pages
+    end
+
+    # PERF we should reduce this to 1 call per moc.
+    persistable_stacks.map do |persistable|
+      saved = persistable.save  # TODO implmeent if-dirty.
+      if saved
+        saved_stacks << stack
+      else
+        pe_warn "stack #{stack.name} didn't save" 
+      end
+    end
+
+    pe_log "saved #{saved_stacks.size} stacks."
   end
   
   def load_stacks
     # fetch CoreDataStack, then -> Stack.
   end
+
+
+  def persistable_pages pages
+    pages.map do |page|
+      p = CoreDataPage.new title:page.title, url:page.url, 
+        last_accessed:Time.new(page.last_accessed_timestamp), 
+        first_accessed:Time.new(page.timestamp)
+      
+      # unfortunate boilerplating for core_data_wrapper.
+      ctx = App.delegate.managedObjectContext
+      ctx.insertObject(p) # inserted into context, but not yet persisted
+      p
+    end
+  end
+  
 end
 
 
