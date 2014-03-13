ac27b45d1bab53053ce47cbd10a01545734a8cd4
Andy Park <sohocoke@gmail.com>
2014-03-13 21:57:46 +0100

    confirm successful import of bblmotionkit upstream changes

---
+++ app/_rm_dep_hack.rb
@@ -0,0 +1,11 @@
+PlatformViewController = NSViewController
+
+
+class MotionViewController < PlatformViewController
+end
+
+class MotionKitViewController < MotionViewController
+end
+
+class PEViewController < MotionKitViewController
+end
