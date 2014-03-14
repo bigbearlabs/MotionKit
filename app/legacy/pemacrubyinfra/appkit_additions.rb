2669d4ab3a5f471546e5cda3edefc6be08619e32
Andy Park <sohocoke@gmail.com>
2014-03-14 16:32:45 +0100

    move out NSApplication.

--- app/legacy/pemacrubyinfra/appkit_additions.rb
+++ app/legacy/pemacrubyinfra/appkit_additions.rb
@@ -5,64 +5,6 @@
 # macruby_framework 'AppKit'
 # macruby_framework 'ExceptionHandling'
 
-class NSApplication
-
-	def activate
-		activateIgnoringOtherApps(true)
-	end
-
-	def name
-		NSRunningApplication.currentApplication.localizedName
-	end
-	
-	def bundle_id
-		NSBundle.mainBundle.bundleIdentifier
-	end
-
-	def pid
-		NSRunningApplication.currentApplication.processIdentifier
-	end
-	
-	def icon
-		applicationIconImage
-	end
-	
-	#= path access
-
-	def app_support_path
-		NSFileManager.defaultManager.privateDataPath
-	end
-
-	def bundle_resources_path
-		NSBundle.mainBundle.resourcePath
-	end
-	
-	#=
-
-	def status_bar_window
-		selection = self.windows.select do |w|
-			w.is_a? NSStatusBarWindow
-		end
-
-		if selection.size != 1
-			pe_warn "unexpected results for #status_bar_window: #{selection}"
-		end
-
-		selection.first
-	end
-
-	#=
-
-	def windows_report
-		ws = windows.collect do |w|
-			w.to_s + ":" + w.title.to_s
-		end
-		ws.to_s + ", keyWindow: " + self.keyWindow.to_s + ", mainWindow: " + self.mainWindow.to_s
-	end
-
-end
-
-
 #=
 
 
@@ -512,19 +454,6 @@ class NSArrayController
 	def empty!
 	 range = NSMakeRange(0, self.arrangedObjects.count)
 	 self.removeObjectsAtArrangedObjectIndexes(NSIndexSet.indexSetWithIndexesInRange(range))
-	end
-end
-
-
-class NSAppleEventDescriptor
-	def url_string
-		# translated from GTM sample code 
-		
-		url_string = self.paramDescriptorForKeyword(KeyDirectObject).stringValue
-		
-		raise "error extracting url from #{self}" unless url_string
-		
-		url_string
 	end
 end
 
