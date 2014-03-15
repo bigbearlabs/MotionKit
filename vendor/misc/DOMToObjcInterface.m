5a5e7eb72c553eba693bd0e583c301cf2ae7c63d
Andy Park <sohocoke@gmail.com>
2014-03-15 15:28:45 +0100

    ignore .idea

--- vendor/misc/DOMToObjcInterface.m
+++ vendor/misc/DOMToObjcInterface.m
@@ -1,1 +1,38 @@
-../../../WebBuddy/PEFramework/PEFramework/DOMToObjcInterface.m
+//
+//  DOMToObjcInterface.m
+//  WebBuddy
+//
+//  Created by ilo-robbie on 16/01/2013.
+//
+//
+
+#import "DOMToObjcInterface.h"
+
+// an indirection to assure that method invocation succeeds from js through DOM window object to a macruby object, set as the callback handler to an instance of this class and implementing the method 'performWebkitInvokableOperation'. work around what seems to be lagging class definitions understood by webkit in commit 9847bc4.
+@implementation DOMToObjcInterface
+
+- (id)initWithCallbackHandler:(id) handler
+{
+	self = [super init];
+	if (self) {
+		self.handler = handler;
+	}
+	return self;
+}
+
+// FIXME exceptions thrown are not reported properly.
+-(id) callback:(NSString*) callbackName {
+	NSLog(@"invoking %@ on %@", callbackName, self.handler);
+	id result = [self.handler performSelector:NSSelectorFromString(callbackName)];
+	
+	return result;
+}
+
++ (BOOL)isSelectorExcludedFromWebScript:(SEL)aSelector {
+	NSLog(@"webkit querying for selector %@", NSStringFromSelector(aSelector));
+
+	// wide-open access.
+	return false;
+}
+
+@end
--- vendor/misc/Environment.h
+++ vendor/misc/Environment.h
@@ -1,1 +1,15 @@
-../../../WebBuddy/PEFramework/PEFramework/Environment.h
+//
+//  Environment.h
+//  WebBuddy
+//
+//  Created by Park Andy on 05/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Environment : NSObject
+
+@property(readonly) NSDictionary* values;
+
+@end
