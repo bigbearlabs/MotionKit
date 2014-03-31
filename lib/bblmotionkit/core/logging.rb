8cdd72294b4188d2b5452877661d298d11f79f0b
Andy Park <sohocoke@gmail.com>
2014-03-31 14:44:31 +0200

    Revert "fix indentation 2."
    This reverts commit c42d4a977593d2fd0a2547d2e259c76c32b4bac0.

--- lib/bblmotionkit/core/logging.rb
+++ lib/bblmotionkit/core/logging.rb
@@ -9,50 +9,50 @@ Log.level = :info
 # set the LOGGING envvar to a csv of log levels to send to NSLog.
 # TODO change envvar value to a csv of log module - level pairs.
 module Logging
-  def log_level
-    :info
-  end
+	def log_level
+		:info
+	end
 
-  def pe_debug( msg )
-    Log.debug msg.to_s
-  end
+	def pe_debug( msg )
+		Log.debug msg.to_s
+	end
+	
+	def pe_log( msg )
+		if log_level == :info
+			Log.info msg.to_s
+		end
+	end
   
-  def pe_log( msg )
-    if log_level == :info
-      Log.info msg.to_s
-    end
-  end
-  
-  def pe_warn( msg, options = {})
+	def pe_warn( msg, options = {})
     if options[:ticket]
       # TODO show message with ticket reporting url.
     end
 
-    Log.warn msg.to_s
-  end
+		Log.warn msg.to_s
+	end
 
   def pe_trace(msg = nil)
-    # if Environment.instance.isDebugBuild
+  	# if Environment.instance.isDebugBuild
     if RUBYMOTION_ENV == "development"
-      stack = $DEBUG ? caller : caller[0..5]
-      pe_log "** TRACE #{msg.to_s} ** #{stack.format_backtrace.join(" - ")}"
+	  	stack = $DEBUG ? caller : caller[0..5]
+	    pe_log "** TRACE #{msg.to_s} ** #{stack.format_backtrace.join(" - ")}"
     end
   end
 
 
-  def pe_report( exception, msg = nil )
-    pe_warn "** Exception ** #{msg ? msg : nil} ** #{exception.inspect} #{exception.description}** backtrace: #{exception.report}"
-    debug exception: exception
-  end
-  
-  def pe_escape_format_specifiers(str)
+	def pe_report( exception, msg = nil )
+		pe_warn "** Exception ** #{msg ? msg : nil} ** #{exception.inspect} #{exception.description}** backtrace: #{exception.report}"
+		debug exception: exception
+	end
+	
+	def pe_escape_format_specifiers(str)
     msg = str.to_s.gsub(/%+?/,"%%")
     msg
-  end
+	end
   
 end
 
 
 class NSObject
-  include Logging
+	include Logging
 end
