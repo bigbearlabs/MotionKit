54d2ae44e04cbf35d5f498f5a54d317d9a67b18b
Andy Park <sohocoke@gmail.com>
2014-04-05 23:45:41 +0200

    #react_to: invoke handler per key path; invoke only when value changed.

--- bblmotionkit/lib/bblmotionkit/core/reactive.rb
+++ bblmotionkit/lib/bblmotionkit/core/reactive.rb
@@ -13,15 +13,15 @@ module Reactive
 
   def react_to( *key_paths, &block )
     key_paths.each do |key_path|
-      observe self, key_path do |k,c,ctx|
-        vals = key_paths.map {|p| self.kvc_get p}
-        # TODO by default, invoke only if value changed.
-        pe_debug "#{self}: #{key_path} changed, reacting with #{vals}"
+      observe self, key_path do |old_val, new_val|
+        if old_val != new_val
+          pe_debug "#{self}: #{key_path} changed, reacting with #{new_val}"
         
-        begin
-          block.call(*vals)
-        rescue Exception => e
-          pe_warn e          
+          begin
+            block.call(new_val)
+          rescue Exception => e
+            pe_warn e          
+          end
         end
       end
     end
