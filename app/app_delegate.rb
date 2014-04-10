418155b4ac583fb30f9dab855bc66b47b050f648
Andy Park <sohocoke@gmail.com>
2014-04-10 22:07:20 +0200

    clean up the app delegate.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,21 @@
-# TODO refactor into MotionKitAppDelegate.
+class AppDelegate < ProMotion::Delegate
 
-class AppDelegate
+  def on_load(application, launchOptions)
+    
+    browser_vc = BrowserViewController.new
+    setup_window browser_vc
+    
+    browser_vc.toggle_input self  # TODO wire up with ui event.
+
+    # browser_vc.load_file 'testfile.html'
+    browser_vc.load_url 'http://flappybird.io'
+
+    
+    true
+  end
+  
+  #=
+
   def load_url url
     browser_vc.load_url url
   end
@@ -11,29 +26,6 @@ class AppDelegate
 
   #= 
 
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-# motion_require '../ProMotion/lib/ProMotion.rb'
-
-# class AppDelegate < ProMotion::Delegate
-#   def on_load(application, launchOptions)
-    
-    browser_vc = BrowserViewController.new
-    setup_window browser_vc
-    
-    browser_vc.toggle_input self  # TODO wire up with ui event.
-
-    # browser_vc.load_file 'testfile.html'
-    browser_vc.load_url 'http://flappybird.io'
-
-
-    # test out a repl.
-    # r = repl self
-    # puts "repl: #{r}"
-    # puts r.evaluateExpression 'self'
-    
-    true
-  end
-  
 
   def setup_window vc
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
