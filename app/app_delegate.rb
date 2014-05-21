3a4e18ea5280a1e9cd2de1c0a892e2f500ba1d84
Andy Park <sohocoke@gmail.com>
2014-05-21 11:47:01 +0200

    plugins/index.html#/sneakers/quiz

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,10 +4,9 @@ class AppDelegate < MotionKitAppDelegate
     
     browser_vc = BrowserViewController.new
     setup_window browser_vc
+
     
-    resource_path = 'http://flappybird.io'
-    
-    browser_vc.load_resource resource_path
+    browser_vc.load_resource 'plugins/index.html#/sneakers/quiz'
     
     # browser_vc.toggle_input self  # TODO wire up with ui event.
 
