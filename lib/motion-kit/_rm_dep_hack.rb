9e6445ba4f2d8b8d2f7aff44704207d99de0b32e
Andy Park <sohocoke@gmail.com>
2014-06-30 19:08:17 +0200

    move _rm_dep_hack to lib/motion-kit

---
+++ lib/motion-kit/_rm_dep_hack.rb
@@ -0,0 +1,15 @@
+# needed to work around dep issues for gem 'ib'
+# require 'ib/outlets'
+
+
+# # TODO move into a file addition next to motionkit.rb
+if BW::App.ios?
+  PlatformViewController =  ProMotion::Screen
+  PlatformView = UIView
+  PlatformWebView = UIWebView
+else
+  PlatformViewController =  NSViewController
+  PlatformView = NSView
+  PlatformWebView = WebView
+end
+
