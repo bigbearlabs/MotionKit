277c9997d0c9622e86f5d69717ec385cbfe820bd
Andy Park <sohocoke@gmail.com>
2014-07-03 01:04:34 +0200

    fix typo.

--- lib/motion-kit/app/ios/app.rb
+++ lib/motion-kit/app/ios/app.rb
@@ -1,9 +1,9 @@
 class MotionKitAppDelegate < ProMotion::Delegate
 
-  def setup_window root_view_controlelr
+  def setup_window root_view_controller
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
-    @window.rootViewController = root_view_controlelr
+    @window.rootViewController = root_view_controller
     @window.rootViewController.wantsFullScreenLayout = true
   end
 
