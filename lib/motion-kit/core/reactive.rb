a6153a87b0230363070d63a49d75084a4fa20514
Andy Park <sohocoke@gmail.com>
2014-07-26 00:56:02 +0200

    #react_to_eager

--- lib/motion-kit/core/reactive.rb
+++ lib/motion-kit/core/reactive.rb
@@ -30,4 +30,21 @@ module Reactive
 
   end
 
+  def react_to_eager( *key_paths, &block )
+    key_paths.each do |key_path|
+      observe self, key_path do |old_val, new_val|
+        if Log.level == :debug
+          pe_debug "#{self}.#{key_path} changed, reacting with #{new_val}"
+        end
+        
+        begin
+          block.call(new_val)
+        rescue Exception => e
+          pe_warn e          
+        end
+      end
+    end
+
+  end
+
 end
